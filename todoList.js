8d4ef551467bc1b5e9bc278d9f36dbf82db03cf5
Sandeep-pohare <sandeeppohare2004@gmail.com>
2025-12-31 22:01:59 +0530

    first commit

---
+++ todoList.js
@@ -0,0 +1,8 @@
+// javascript for todo 
+let btn = document.querySelector("button");
+
+
+btn.addEventListener("click", function (){
+    let inp = document.querySelector("input");
+    console.log(inp.value)
+})
